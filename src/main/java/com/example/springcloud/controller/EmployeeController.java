808e7212ecb0e45220e878f5a199f63ed53aac02
Markus28 <guidoceci28@gmail.com>
2019-05-13 15:26:23 +0200

    added get mapping searching for gle entity

--- src/main/java/com/example/springcloud/controller/EmployeeController.java
+++ src/main/java/com/example/springcloud/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springcloud.model.Employee;
@@ -18,6 +19,11 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> listAllEmployee(){
 		return employeeService.findAllEmployee();
+	}
+	
+	@GetMapping("/employees/{id}")
+	public Employee getEmployeeById(@PathVariable Long id) {
+		return employeeService.findById(id);
 	}
 
 }
